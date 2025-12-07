a5eae0d6658e962421acc4366d57ffe800dafcfb
SmartFlow Developer <adekunle@smartflow.dev>
2025-12-07 16:12:59 +0100

    config: Optimize webpack externals and add image domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
+  },
+  images: {
+    domains: ['basescan.org'],
   },
 }
 
